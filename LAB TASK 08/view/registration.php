09c2d8932c16ff1ad331ed17210edb01e6af5327
sk shadik <57946665+SK-SHADIK@users.noreply.github.com>
2023-04-26 20:43:54 +0600

    Lab Task 08 Registration JS Validation

--- LAB TASK 08/view/registration.php
+++ LAB TASK 08/view/registration.php
@@ -26,7 +26,7 @@ require '../controller/registrationController.php';
    
 <div class="form-container2">
 
-   <form action="" method="post">
+   <form action="" method="post" id="myForm">
       <h3>Register Now</h3>
       <?php
       if(isset($error)){
@@ -35,9 +35,9 @@ require '../controller/registrationController.php';
          };
       };
       ?>
-      <input type="text" name="name" placeholder="Enter your name"><br>
-      <input type="email" name="email" placeholder="Enter your email"><br>
-      <input type="password" name="password" placeholder="Enter your password"><br>
+      <input type="text" name="name" id="name" placeholder="Enter your name"><br>
+      <input type="text" name="email" id="email" placeholder="Enter your email"><br>
+      <input type="password" id="password" name="password" placeholder="Enter your password"><br>
       <input type="password" name="cpassword" placeholder="Confirm your password"><br>
       <label for="gender" style="font-size:20px; float:left;">Gender</label><br>
       <input type="radio" name="gender" value="Male">
@@ -46,8 +46,8 @@ require '../controller/registrationController.php';
       <label for="female" >Female</label>
       <input type="radio" name="gender" value="Other">
       <label for="other">Other</label><br>
-      <input type="text" name="address" placeholder="Enter your address"><br>
-      <input type="text" name="contact" placeholder="Enter your contact"><br>
+      <input type="text" id="address" name="address" placeholder="Enter your address"><br>
+      <input type="text" id="contact" name="contact" placeholder="Enter your contact"><br>
       
       <input type="submit" name="submit" value="Register" class="form-btn">
       <p>I already have an account? <a href="login.php">Login</a></p>
@@ -58,3 +58,58 @@ require '../controller/registrationController.php';
 
 </body>
 </html>
+<script>
+  const myForm = document.getElementById('myForm');
+
+  myForm.addEventListener('submit', function (event) {
+    const nameInput = document.getElementById('name');
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+    const phoneInput = document.getElementById('contact');
+    const addressInput = document.getElementById('address');
+
+    // Check for empty fields
+    if (nameInput.value.trim() === '' ||
+      emailInput.value.trim() === '' ||
+      passwordInput.value.trim() === '' ||
+      addressInput.value.trim() === '' ||
+      phoneInput.value.trim() === '') {
+      alert('Please fill in all fields.');
+      event.preventDefault();
+      return;
+    }
+
+    // Check name field
+    const nameRegex = /^[a-zA-Z ]{2,30}$/;
+    if (!nameRegex.test(nameInput.value)) {
+      alert('Please enter a valid name (2-30 letters, no numbers or special characters).');
+      event.preventDefault();
+      return;
+    }
+
+    // Check email field
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(emailInput.value)) {
+      alert('Please enter a valid email address.');
+      event.preventDefault();
+      return;
+    }
+
+    // Check password field
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
+    if (!passwordRegex.test(passwordInput.value)) {
+      alert('Please enter a valid password (8+ characters, at least one uppercase letter, one lowercase letter, one number, and one special character).');
+      event.preventDefault();
+      return;
+    }
+
+    // Check phone number field
+    const phoneRegex = /^\d{11}$/;
+    if (!phoneRegex.test(phoneInput.value)) {
+      alert('Please enter a valid 11-digit phone number (numbers only, no dashes or spaces).');
+      event.preventDefault();
+      return;
+    }
+  });
+
+</script>
